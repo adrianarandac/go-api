a9feb1cca617d5345d8e3db59209cfc263841ee5
Adrián Aranda <adrienaranda@gmail.com>
2022-03-11 13:29:47 +0100

    Checkout and return finished

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"net/http"
 	"errors"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,7 +16,7 @@ type book struct{
 
 var books = []book{
 	{ID: "1", Title: "Don Quijote", Author: "Cervantes", Quantity: 4},
-	{ID: "2", Title: "1984", Author: "Orson Welles", Quantity: 20},
+	{ID: "2", Title: "1984", Author: "Orson Welles", Quantity: 1},
 	{ID: "3", Title: "Holy Bible", Author: "Lotta People", Quantity: 69},
 }
 
@@ -35,6 +35,50 @@ func bookById (c *gin.Context)  {
 
 	c.IndentedJSON(http.StatusOK, book) 
 }
+
+//CHECKOUT
+func checkoutBook(c *gin.Context){
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+		return
+	}
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H {"message": "book out of stock"})
+		return
+	}
+	book.Quantity--
+	c.IndentedJSON(http.StatusOK, book) 
+}
+
+//CHECKOUT
+func returnBook(c *gin.Context){
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "book not found"})
+		return
+	}
+
+	book.Quantity++
+	c.IndentedJSON(http.StatusOK, book) 
+}
+
 
 
 func getBookById (id string) (*book, error) {
@@ -60,6 +104,8 @@ func main () {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.GET("/books/:id", bookById)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
 	router.POST("/books", createBook)
 	router.Run("localhost:8080")
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+# API-GO
+
+API-GO is my first ever Go api. Its purpose is purely educational.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
